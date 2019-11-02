e2fd773ce5e46e51727ae824bd212fcdbc8f4a4b
Kamil <cfvbnm882@gmail.com>
2019-11-02 12:42:09 +0100

    Algorytm sortowania BubbleSort

---
+++ BubbleSortClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmySortujace
+{
+    class BubbleSortClass
+    {
+        public void BubbleSort(int[] numbers)
+        {
+            int tmp;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        tmp = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = tmp;
+                    }
+
+                }
+            }
+        }
+    }
+}
